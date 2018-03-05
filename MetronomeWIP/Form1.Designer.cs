6d355fd4b7b025bcb86528a3bbc9cbe29920abd4
unknown <jslike1@kent.edu>
2018-03-04 20:20:44 -0500

    Updated form to include reps

--- MetronomeWIP/Form1.Designer.cs
+++ MetronomeWIP/Form1.Designer.cs
@@ -44,14 +44,18 @@ namespace MetronomeWIP
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(271, 282);
+            this.button1.Location = new System.Drawing.Point(181, 183);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 86);
+            this.button1.Size = new System.Drawing.Size(101, 56);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start/Stop";
             this.button1.UseVisualStyleBackColor = false;
@@ -59,52 +63,58 @@ namespace MetronomeWIP
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(630, 177);
+            this.textBox1.Location = new System.Drawing.Point(420, 115);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(736, 180);
+            this.label1.Location = new System.Drawing.Point(491, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.Size = new System.Drawing.Size(106, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Target Tempo (BPM)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(203, 177);
+            this.textBox2.Location = new System.Drawing.Point(135, 115);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
             this.textBox2.TabIndex = 3;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(284, 42);
+            this.textBox3.Location = new System.Drawing.Point(146, 27);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBoxMeasures
             // 
-            this.textBoxMeasures.Location = new System.Drawing.Point(524, 42);
+            this.textBoxMeasures.Location = new System.Drawing.Point(257, 27);
+            this.textBoxMeasures.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxMeasures.Name = "textBoxMeasures";
-            this.textBoxMeasures.Size = new System.Drawing.Size(100, 26);
+            this.textBoxMeasures.Size = new System.Drawing.Size(68, 20);
             this.textBoxMeasures.TabIndex = 5;
             this.textBoxMeasures.TextChanged += new System.EventHandler(this.textBoxMeasure_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 180);
+            this.label2.Location = new System.Drawing.Point(21, 117);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.Size = new System.Drawing.Size(111, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "Starting Tempo (BPM)";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -112,9 +122,10 @@ namespace MetronomeWIP
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(267, 19);
+            this.label3.Location = new System.Drawing.Point(143, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 20);
+            this.label3.Size = new System.Drawing.Size(96, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Beats per Measure";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -122,18 +133,20 @@ namespace MetronomeWIP
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(508, 19);
+            this.label4.Location = new System.Drawing.Point(254, 12);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(126, 20);
+            this.label4.Size = new System.Drawing.Size(84, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "Measures Count";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(502, 281);
+            this.button2.Location = new System.Drawing.Point(335, 183);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 87);
+            this.button2.Size = new System.Drawing.Size(101, 57);
             this.button2.TabIndex = 9;
             this.button2.Text = "Pause/Unpause";
             this.button2.UseVisualStyleBackColor = true;
@@ -141,18 +154,20 @@ namespace MetronomeWIP
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(309, 177);
+            this.textBox5.Location = new System.Drawing.Point(206, 115);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(315, 26);
+            this.textBox5.Size = new System.Drawing.Size(211, 20);
             this.textBox5.TabIndex = 10;
             this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(320, 154);
+            this.label5.Location = new System.Drawing.Point(213, 100);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(292, 20);
+            this.label5.Size = new System.Drawing.Size(196, 13);
             this.label5.TabIndex = 11;
             this.label5.Text = "Intermediate Tempos (BPM, BPM, BPM)";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -160,9 +175,10 @@ namespace MetronomeWIP
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(508, 71);
+            this.label6.Location = new System.Drawing.Point(254, 49);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(145, 20);
+            this.label6.Size = new System.Drawing.Size(97, 13);
             this.label6.TabIndex = 12;
             this.label6.Text = "Current Measure: 1";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -170,17 +186,46 @@ namespace MetronomeWIP
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(280, 71);
+            this.label7.Location = new System.Drawing.Point(143, 49);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(117, 20);
+            this.label7.Size = new System.Drawing.Size(78, 13);
             this.label7.TabIndex = 13;
             this.label7.Text = "Current Beat: 1";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(374, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Reps Count";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(377, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(377, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Current Rep: 1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(896, 380);
+            this.ClientSize = new System.Drawing.Size(597, 247);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -195,6 +240,7 @@ namespace MetronomeWIP
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -220,6 +266,9 @@ namespace MetronomeWIP
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label9;
     }
 }
 
